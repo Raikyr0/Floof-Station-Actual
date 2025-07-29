bc1424e57b3af9939974d03ee76805a1f8785ffe
Raikyr0 <Kurohana@hotmail.com.au>
2025-07-29 13:07:29 +1000

    Update HideContrabandContentComponent.cs

--- Content.Shared/_Goobstation/Contraband/HideContrabandContentComponent.cs
+++ Content.Shared/_Goobstation/Contraband/HideContrabandContentComponent.cs
@@ -1,4 +1,4 @@
-namespace Content.Goobstation.Shared.Contraband;
+namespace Content.Shared._Goobstation.Contraband;
 
 /// <summary>
 /// added to items that will hide items inside from contraband detection.
