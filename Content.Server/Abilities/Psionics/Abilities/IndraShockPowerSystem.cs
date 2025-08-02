59df0eab2ca62c6546bf1224aa4d0b6ec35e2b5a
Raikyr0 <Kurohana@hotmail.com.au>
2025-08-02 15:08:43 +1000

    Update IndraShockPowerSystem.cs

--- Content.Server/Abilities/Psionics/Abilities/IndraShockPowerSystem.cs
+++ Content.Server/Abilities/Psionics/Abilities/IndraShockPowerSystem.cs
@@ -25,6 +25,13 @@ namespace Content.Server.Abilities.Psionics
             if (!_psionics.OnAttemptPowerUse(args.Performer, "Indra Shock"))
                 return;
 
+            args.ModifiedAmplification = _psionics.ModifiedAmplification(uid, component);
+            args.ModifiedDampening = _psionics.ModifiedDampening(uid, component);
+
+            if (!args.Immediate)
+                AttemptDoAfter(uid, component, args);
+            else ActivatePower(uid, component, args);
+            
             _beam.TryCreateBeam(args.Performer, args.Target, "LightningNoospheric");
 
             _stunSystem.TryParalyze(args.Target, TimeSpan.FromSeconds(2.5), false);
@@ -33,5 +40,48 @@ namespace Content.Server.Abilities.Psionics
             _psionics.LogPowerUsed(args.Performer, "Indra Shock");
             args.Handled = true;
         }
-    }
+        
+        private void AttemptDoAfter(EntityUid uid, PsionicComponent component, IndraShockPowerActionEvent args)
+        {
+            var ev = new IndraShockDoAfterEvent(_gameTiming.CurTime);
+            if (args.HealingAmount is not null)
+                ev.HealingAmount = args.HealingAmount;
+            
+            ev.ModifiedAmplification = args.ModifiedAmplification;
+            ev.ModifiedDampening = args.ModifiedDampening;
+        }
+
+        private void OnDoAfter(EntityUid uid, PsionicComponent component, IndraShockPowerDoAfterEvent args)
+        {
+            // It's entirely possible for the caster to stop being Psionic(due to mindbreaking) mid cast
+            if (component is null)
+                return;
+            component.DoAfter = null;
+
+            // The target can also cease existing mid-cast
+            // Or the DoAfter is cancelled(such as if the caster moves).
+            if (args.Target is null
+                || args.Cancelled)
+                return;
+
+            if (!TryComp<DamageableComponent>(args.Target.Value, out var damageableComponent))
+                return;
+
+            if (args.HealingAmount is not null)
+                _damageable.TryChangeDamage(args.Target.Value, args.HealingAmount * args.ModifiedAmplification, true, false, damageableComponent, uid);
+        }
+        
+        // This would be the same thing as OnDoAfter, except that here the target isn't nullable, so I have to reuse code with different arguments.
+        private void ActivatePower(EntityUid uid, PsionicComponent component, IndraShockPowerActionEvent args)
+        {
+            if (component is null)
+                return;
+
+            if (!TryComp<DamageableComponent>(args.Target, out var damageableComponent))
+                return;
+
+            if (args.HealingAmount is not null)
+                _damageable.TryChangeDamage(args.Target, args.HealingAmount * args.ModifiedAmplification, true, false, damageableComponent, uid);
+        }
+    }    
 }
