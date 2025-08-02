88c678affb0a324c0605c57d1eea8914c1224923
Raikyr0 <Kurohana@hotmail.com.au>
2025-08-02 14:54:31 +1000

    Update IndraShockPowerActionEvent.cs

--- Content.Shared/Actions/Events/IndraShockPowerActionEvent.cs
+++ Content.Shared/Actions/Events/IndraShockPowerActionEvent.cs
@@ -1,2 +1,22 @@
+using Content.Shared.Damage;
+
 namespace Content.Shared.Actions.Events;
-public sealed partial class IndraShockPowerActionEvent : EntityTargetActionEvent {}
+public sealed partial class IndraShockPowerActionEvent : EntityTargetActionEvent 
+{
+    /// <summary>
+    ///     Caster's Amplification that has been modified by the results of a MoodContest.
+    /// </summary>
+    public float ModifiedAmplification = default!;
+
+    /// <summary>
+    ///     Caster's Dampening that has been modified by the results of a MoodContest.
+    /// </summary>
+    public float ModifiedDampening = default!;
+
+    [DataField]
+    public DamageSpecifier? HealingAmount = default!;
+
+    /// Controls whether or not a power fires immediately and with no DoAfter
+    [DataField]
+    public bool Immediate;
+}
