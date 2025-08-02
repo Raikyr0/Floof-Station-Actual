b603efd691a7073f77a5d857c03e9a9594808b62
Raikyr0 <Kurohana@hotmail.com.au>
2025-08-02 15:15:50 +1000

    Update IndraShockPowerActionEvent.cs

--- Content.Shared/Actions/Events/IndraShockPowerActionEvent.cs
+++ Content.Shared/Actions/Events/IndraShockPowerActionEvent.cs
@@ -20,30 +20,3 @@ public sealed partial class IndraShockPowerActionEvent : EntityTargetActionEvent
     [DataField]
     public bool Immediate;
 }
-
-[Serializable, NetSerializable]
-public sealed partial class IndraShockDoAfterEvent : DoAfterEvent
-{
-    [DataField(required: true)]
-    public TimeSpan StartedAt;
-
-    [DataField]
-    public DamageSpecifier? HealingAmount = default!;
-
-    /// <summary>
-    ///     Caster's Amplification that has been modified by the results of a MoodContest.
-    /// </summary>
-    public float ModifiedAmplification = default!;
-
-    /// <summary>
-    ///     Caster's Dampening that has been modified by the results of a MoodContest.
-    /// </summary>
-    public float ModifiedDampening = default!;
-
-    public IndraShockDoAfterEvent(TimeSpan startedAt)
-    {
-        StartedAt = startedAt;
-    }
-
-    public override DoAfterEvent Clone() => this;
-}
