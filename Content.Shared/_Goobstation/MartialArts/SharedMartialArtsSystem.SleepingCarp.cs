806d5195fd4ba43409a82e6e421f877d21a93268
Raikyr0 <Kurohana@hotmail.com.au>
2025-09-10 14:40:31 +1000

    Update SharedMartialArtsSystem.SleepingCarp.cs

--- Content.Shared/_Goobstation/MartialArts/SharedMartialArtsSystem.SleepingCarp.cs
+++ Content.Shared/_Goobstation/MartialArts/SharedMartialArtsSystem.SleepingCarp.cs
@@ -10,6 +10,7 @@ using Content.Shared.Movement.Pulling.Components;
 using Content.Shared.Popups;
 using Content.Shared.Weapons.Reflect;
 using Robust.Shared.Audio;
+using Content.Shared.NPC.Systems;
 
 namespace Content.Shared._Goobstation.MartialArts;
 
