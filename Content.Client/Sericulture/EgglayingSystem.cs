4d651384009e3befddc7cd5e857bc8e9c0d01977
Raikyr0 <Kurohana@hotmail.com.au>
2025-09-21 12:05:22 +1000

    Add files via upload

---
+++ Content.Client/Sericulture/EgglayingSystem.cs
@@ -0,0 +1,9 @@
+using Content.Shared.Sericulture;
+
+namespace Content.Client.Sericulture;
+
+/// <summary>
+/// <So, the reason why this is being added is because if you take 2 things that add Sericulture it breaks.>
+/// <inheritdoc/>
+/// </summary>
+public sealed partial class EgglayingSystem : SharedEgglayingSystem { }
