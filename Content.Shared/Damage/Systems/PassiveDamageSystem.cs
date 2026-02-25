82122f33d38ffdfec40dc19de8fb6ffc436d2529
Raikyr0 <Kurohana@hotmail.com.au>
2026-02-26 09:49:58 +1100

    Update PassiveDamageSystem.cs

--- Content.Shared/Damage/Systems/PassiveDamageSystem.cs
+++ Content.Shared/Damage/Systems/PassiveDamageSystem.cs
@@ -18,7 +18,17 @@ public sealed class PassiveDamageSystem : EntitySystem
         SubscribeLocalEvent<PassiveDamageComponent, PassiveHealingComponent, ChronicDamageComponent, MapInitEvent>(OnPendingMapInit);
     }
 
-    private void OnPendingMapInit(EntityUid uid, PassiveDamageComponent ,PassiveHealingComponent, ChronicDamageComponent component, MapInitEvent args)
+    private void OnPendingMapInit(EntityUid uid, PassiveDamageComponent component, MapInitEvent args)
+    {
+        component.NextDamage = _timing.CurTime + TimeSpan.FromSeconds(1f);
+    }
+    
+    private void OnPendingMapInit(EntityUid uid, PassiveHealingComponent component, MapInitEvent args)
+    {
+        component.NextDamage = _timing.CurTime + TimeSpan.FromSeconds(1f);
+    }
+
+     private void OnPendingMapInit(EntityUid uid, ChronicDamageComponent component, MapInitEvent args)
     {
         component.NextDamage = _timing.CurTime + TimeSpan.FromSeconds(1f);
     }
